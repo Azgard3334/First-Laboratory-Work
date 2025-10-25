dede0b5a6a1da67ab984452e527ea3a2b0f5d85a
Askar <rizatdinov06@mail.ru>
2025-10-25 19:21:01 +0300

    add overflow exception handling

--- main.cpp
+++ main.cpp
@@ -13,17 +13,21 @@ int main() {
 	u_t a = 0, b = 0, c = 0;
 	std::cin >> b >> c;
 	size_t count = 0;
+	try {
 	while (std::cin >> a) {
 		count = isPyth(a, b, c) ? 1 : 0;
 		c = b;
 		b = a;
 	}
+	} catch (const std::logic_error &e) {
+		std::cerr << e.what() << std::endl;
+		return 2;
+	}
 
 	if (std::cin.eof()) {
 		std::cout << count;
 		std::cout << '\n';
-	}
-	else if (std::cin.fail()) {
+	} else if (std::cin.fail()) {
 		std::cout << "Error\n";
 		return 1;
 	}

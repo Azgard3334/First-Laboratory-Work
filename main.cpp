e25d4155592105df4160bbe6412b56329cc3ea6a
Askar <rizatdinov06@mail.ru>
2025-10-25 12:26:43 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstddef>
+
+int main() {
+	int a = 0; size_t count = 0;
+	while (!std::cin.eof()) {
+		std::cin >> a;
+		if (std::cin.bad()) {
+			return 1;
+		}
+		if (!std::cin.eof() && std::cin.fail()) {
+			return 2;
+		}
+		++count;
+	}
+	std::cout << count << std::endl;
+	return 0;
+}
